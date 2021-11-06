2de9b4dcc7760f41f9622935c5a8755698b8777b
Jackal42 <91426054+Jackal-42@users.noreply.github.com>
2021-11-06 00:12:34 +0000

    stuff

--- JackalScript/images.js
+++ JackalScript/images.js
@@ -6,5 +6,5 @@ var images = [
 
 
 for(var i = 0; i < images.length; i++){
-  eval("var "+images[i]+" = document.createElement(\'img\');"+images[i]+".style.display = \'none;\';"+images[i]+".src = \'assets/"+images[i]+".png\';"+images[i]+".alt = \'alt text\'")
+  eval("var "+images[i]+" = document.createElement(\'img\');"+images[i]+".style.display = \'none;\';"+images[i]+".src = \'docs/assets/"+images[i]+".png\';"+images[i]+".alt = \'alt text\'")
 }
